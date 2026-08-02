c46fa4f12ac5dc0e8f630064e95ecac3a28442b7
Victória Gomes <victoria.oliveiragomes@gmail.com>
2026-08-02 18:53:10 -0400

    feat: refactor skills loading implementation to align with Angular best practices

--- src/app/technical-skills/technical-skills.ts
+++ src/app/technical-skills/technical-skills.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Skill } from "./components/skill/skill";
 import { TranslatePipe, TranslateService } from "@ngx-translate/core";
-import { faCode, faDatabase, faMicrochip, faServer, faTerminal, faWrench } from '@fortawesome/free-solid-svg-icons';
-import { SkillItem } from './components/skill/skill-item';
+import { faCode, faDatabase, faMicrochip, faServer, faTerminal } from '@fortawesome/free-solid-svg-icons';
 import { IconDefinition } from '@fortawesome/angular-fontawesome';
 import { faCloud } from '@fortawesome/free-regular-svg-icons';
 import { SectionHeader } from '../shared/components/section-header/section-header';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-technical-skills',
@@ -17,8 +17,7 @@ import { SectionHeader } from '../shared/components/section-header/section-heade
   templateUrl: './technical-skills.html',
   styleUrl: './technical-skills.sass',
 })
-export class TechnicalSkills implements OnInit {
-  protected skills: Record<string, SkillItem> | null = null;
+export class TechnicalSkills {
   protected readonly icons: Record<string, IconDefinition> = {
     "programmingLanguages": faCode,
     "frontEndDevelopment": faMicrochip,
@@ -27,18 +26,12 @@ export class TechnicalSkills implements OnInit {
     "dataAndSearch": faDatabase,
     "engineeringTools": faTerminal
   }
-  protected readonly faWrench = faWrench;
 
-  constructor(private translateService: TranslateService) {
-  }
-
-  ngOnInit(): void {
-    this.translateService.get(
-      'technical-skills.list'
-    ).subscribe(skills => {
-      this.skills = skills;
-    });
-  }
+  private readonly translate = inject(TranslateService);
 
 
+  protected readonly skills = toSignal(
+    this.translate.stream('technical-skills.list'),
+    {initialValue: null}
+  );
 }
