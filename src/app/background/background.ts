bac368879951afda3985412115fb1255305eebd7
Victória Gomes <victoria.oliveiragomes@gmail.com>
2026-08-02 17:56:29 -0400

    fix: remove commented-out code, unused files, and obsolete configuration

--- src/app/background/background.ts
+++ src/app/background/background.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { TranslatePipe, TranslateService } from '@ngx-translate/core';
+import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 import { SectionHeader } from '../shared/components/section-header/section-header';
 import { Infocard } from '../shared/components/infocard/infocard';
 import { ToolChip } from '../shared/components/tool-chip/tool-chip';
-import { TimelineItem } from './timeline-item';
 
 @Component({
   selector: 'app-background',
@@ -16,43 +15,6 @@ import { TimelineItem } from './timeline-item';
   templateUrl: './background.html',
   styleUrl: './background.sass',
 })
-export class Background implements OnInit {
-  protected readonly educationListKey: string = 'background.education.list';
-  protected readonly professionalExperienceListKey: string = 'background.experience.list';
-  protected professionalExperienceTimelineItems: TimelineItem[] = [];
-  protected educationTimelineItems: TimelineItem[] = [];
+export class Background {
 
-  constructor(private translateService: TranslateService) {
-  }
-
-  ngOnInit(): void {
-    this.translateService
-      .get([this.educationListKey, this.professionalExperienceListKey])
-      .subscribe(translations => {
-          // @ts-ignore
-          translations[this.educationListKey].forEach(education => {
-            this.educationTimelineItems.push({
-              title: education.title,
-              subtitle: education.subtitle,
-              description: education.university,
-              startDate: education.startDate,
-              endDate: education.endDate,
-              location: education.location
-            });
-          });
-
-          // @ts-ignore
-          translations[this.professionalExperienceListKey].forEach(experience => {
-            this.professionalExperienceTimelineItems.push({
-              title: experience.title,
-              description: experience.company,
-              startDate: experience.startDate,
-              endDate: experience.endDate,
-              location: experience.location,
-              details: experience.details
-            });
-          });
-        }
-      );
-  }
 }
