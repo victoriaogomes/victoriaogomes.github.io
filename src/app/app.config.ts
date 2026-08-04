8821a2e4b719d84dd9872cc81c669cddff9691a2
Victória Gomes <victoria.oliveiragomes@gmail.com>
2026-08-03 21:29:01 -0400

    fix: remove unused router config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,4 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import {provideHttpClient} from '@angular/common/http';
 import {provideTranslateService} from '@ngx-translate/core';
 import {provideTranslateHttpLoader} from '@ngx-translate/http-loader';
@@ -9,7 +6,6 @@ import {provideTranslateHttpLoader} from '@ngx-translate/http-loader';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
     provideHttpClient(),
     provideTranslateService({
       loader: provideTranslateHttpLoader({
